d5ad4d3758309786c7c332de9659a3cbdd3f6e66
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-05-23 23:24:25 -0400

    Update cards display and controls for mobile.

--- src/components/CardsDisplay/CardsControls.tsx
+++ src/components/CardsDisplay/CardsControls.tsx
@@ -1,6 +1,20 @@
-import { ActionIcon, Group, Text } from '@mantine/core';
+import { ActionIcon, createStyles, Group, Text } from '@mantine/core';
 import * as React from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+const useStyles = createStyles((theme) => ({
+    root: {
+        width: '100%',
+        backgroundColor: '#414F5A',
+        borderRadius: '8px',
+
+        [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+            position: 'fixed',
+            bottom: 0,
+            borderRadius: 0
+        },
+    }
+}));
 
 interface Props {
     total: number,
@@ -10,8 +24,11 @@ interface Props {
 }
 
 const CardsControls = ({ total, current, goBack, goNext }: Props) => {
+
+    const { classes } = useStyles();
+
     return (
-        <Group noWrap align='center' position='apart' p={'xs'} sx={{ backgroundColor: '#414F5A', borderRadius: '8px', width: '100%' }}>
+        <Group className={classes.root} noWrap align='center' position='apart' p={'xs'}>
             <ActionIcon variant='transparent' sx={{ color: current > 1 ? '#B1C6D8' : '#5C6C78' }} size={60} onClick={goBack}>
                 <MdChevronLeft size={60} />
             </ActionIcon>
--- src/components/CardsDisplay/InfoCard.tsx
+++ src/components/CardsDisplay/InfoCard.tsx
@@ -1,20 +1,44 @@
 import * as React from 'react';
-import { Box, Group, Paper, Stack, Text, Title } from '@mantine/core';
+import { Box, createStyles, Group, Paper, Stack, Text, Title } from '@mantine/core';
 import { IStudyCard } from '../../types';
 
+const useStyles = createStyles((theme) => ({
+    root: {
+        width: '40rem',
+        minHeight: '20rem',
+        backgroundColor: theme.colors.gray[0],
+        color: theme.colors.gray[7],
+
+        [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+            width: '100%'
+        },
+    },
+    numLabel: {
+        width: '2rem',
+        height: '2rem',
+        border: '1px solid',
+        borderColor: theme.colors.gray[4],
+        borderRadius: '9999px',
+        userSelect: 'none'
+    }
+}));
+
 const InfoCard = ({ order, title, text }: IStudyCard) => {
+
+    const { classes } = useStyles();
+
     return (
-        <Paper p={30} shadow={'lg'} radius={'lg'} sx={(theme) => ({ width: '40rem', minHeight: '20rem', backgroundColor: theme.colors.gray[0], color: theme.colors.gray[7] })}>
+        <Paper className={classes.root} p={30} shadow={'lg'} radius={'lg'}>
             <Stack >
                 <Group noWrap align={'center'} spacing='sm'>
                     <Group
+                        className={classes.numLabel}
                         noWrap
                         align={'center'}
                         position='center'
                         p={4}
-                        sx={(theme) => ({ borderRadius: '9999px', border: '1px solid', borderColor: theme.colors.gray[4], width: '2rem', height: '2rem' })}
                     >
-                        <Text color='blue' weight={'600'} sx={{ userSelect: 'none' }}> {order! > 9 ? order : '0' + order} </Text>
+                        <Text color='blue' weight={'600'}> {order! > 9 ? order : '0' + order} </Text>
                     </Group>
                     <Title order={4}> {title} </Title>
                 </Group>
